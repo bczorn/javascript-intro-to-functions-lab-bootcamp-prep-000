e8aa135ab0d31ed1bbc6fb3caf944059850ed844
Becky Zorn <bczorn@gmail.com>
2019-07-24 07:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (`string`.toLowerCase())
+  if (`string`.toLowerCase() === `string`)
   return "I can't hear you!"
-  if (`string` === `string`.toUpperCase())
+  if (`string`.toUpperCase() === `string`)
   return "YES INDEED!"
   
 }
